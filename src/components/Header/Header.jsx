2424763d247751873b9a02d9f032449fbb6835da
Tiago <tiago.4lexandre@gmail.com>
2025-05-07 12:06:49 -0300

    feat: adiciona logo e botão de acessibilidade ouditiva no header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,24 @@
+import { LuEar } from "react-icons/lu";
 import "./Header.css";
+import logo from "../../assets/doctoralia-logo.png";
 
 function Header() {
-    return (
-        <header className="doctoralia-header">
-            <h1 className="logo">Doctoralia</h1>
-            <nav className="main-nav">
-                <button className="nav-button">Entrar</button>
-                <button className="nav-button primary">Cadastre-se</button>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="doctoralia-header">
+      <div className="logo-container">
+        <img src={logo} alt="Doctoralia Logo" className="logo-img" />
+        <h1 className="logo-text">Doctoralia</h1>
+      </div>
+
+      <nav className="main-nav">
+        <div className="nav-button orelha">
+          <LuEar className="orelha-icon" />
+        </div>
+        <button className="nav-button">Entrar</button>
+        <button className="nav-button primary">Cadastre-se</button>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
